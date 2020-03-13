bf29f9707b7befa6a5efa573bb1c690e014e0534
roosap <roosa.paivansalo@gmail.com>
2020-03-13 21:23:44 +0000

    Initial commit

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for haskell-play
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import Lib
+
+main :: IO ()
+main = someFunc
+
+doubleMe x = x + x
+
+doubleUs x y = x*2 + y*2
+
+doubleUsAgain x y z = doubleMe x + doubleMe y + doubleMe z
+
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x*2
+numbers = [1, 2, 3]
+moreNumbers = [4, 5, 6]
+
+allNumbers arr1 arr2 = arr1 ++ arr2
+
+hello = "hello"
+world = "world"
+
+helloWorld x y = x ++ y
